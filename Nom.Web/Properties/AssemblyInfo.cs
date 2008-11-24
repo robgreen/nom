5a12074374e784f8affa0f6926d6037ec14cbaf0
unknown <me@robgreen.org>
2008-11-24 13:02:38 +0000

    Added jquery library and automated the query of lat and lng co-ordinates when entering postcode.

--- Nom.Web/Properties/AssemblyInfo.cs
+++ Nom.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LBi")]
 [assembly: AssemblyProduct("Nom.Web")]
-[assembly: AssemblyCopyright("Copyright © LBi 2008")]
+[assembly: AssemblyCopyright("Copyright © Rob Green 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Nom.Web/Resources/JS/Nom.Groups.js
+++ Nom.Web/Resources/JS/Nom.Groups.js
@@ -1,6 +1,24 @@
 ﻿Nom.Groups = {
+	Init: function() {
+		$(function() {
+			GeoLookupInit();
+		});
+	},
+	GeoLookupInit: function() {
+		var postCodes = $("#txtPostalCode");
+		var postCode;
+		
+		if (postCodes.length > 0)
+		{
+			postCode = postCodes[0];
+			$(postCode).keyup(function() {
+				if (this.value.length > 2)
+					Nom.Groups.GeoLookupCall();
+			});
+		}
+	},
 	GeoLookupCall: function() {
-		var postCode = document.getElementById("txtPostalCode");
+		var postCode = $("#txtPostalCode")[0];
 		if (postCode != null)
 		{
 			Nom.Geo.FindLatLonPair(postCode.value, Nom.Groups.GeoLookupCallBack);
@@ -9,11 +27,13 @@
 	GeoLookupCallBack: function() {
 		if (Nom.Geo.LastQuery != null)
 		{
-			var txtLat = document.getElementById("hdnLat");
+			var txtLat = $("#hdnLat")[0];
 			if (txtLat != null) txtLat.value = Nom.Geo.LastQuery.Lat;
 
-			var txtLng = document.getElementById("hdnLng");
+			var txtLng = $("#hdnLng")[0];
 			if (txtLng != null) txtLng.value = Nom.Geo.LastQuery.Lng;
 		}
 	}
 }
+
+Nom.Groups.Init();
